f21c34cfa5d0d3d33d64a96d5be544e53a9d6819
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-07-23 15:40:12 -0400

    Fix scripts

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ sudo systemctl enable obamabot.service
 echo Enabled Obamabot service
 
 echo Installing libraries
-sudo apt install openjdk-11-jre-headless jq curl bash maven
+sudo apt install openjdk-11-jre-headless jq curl bash maven screen
 
 echo Adding cronjob
 cronjob="*/5 * * * * bash '$(pwd)/cronscript.sh'"
 (crontab -u root -l; echo "$cronjob" ) | crontab -u root -
 echo Finished adding cronjob
 
-./cronscript.sh
+bash ./cronscript.sh
 echo Run with "sudo service obamabot start"
