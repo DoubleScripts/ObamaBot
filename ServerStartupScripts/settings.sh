ce08ccb6a6f4ad377f13907bda2af694437a71f8
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2020-07-23 17:33:38 -0400

    Fix scripts and module setup

---
+++ ServerStartupScripts/settings.sh
@@ -0,0 +1,13 @@
+export JAR_VER="1.0-SNAPSHOT"
+export JARFILE="Obama-${JAR_VER}.jar"
+export JAVACMD="java"
+
+export SERVICE_NAME="obamabot"
+
+# By default, this uses Aikar's flags as of 7/16/2020. Change or remove them if needed
+export RAM_USAGE="2G"
+export JVM_ARGUMENTS="-Xms${RAM_USAGE} -Xmx${RAM_USAGE} -jar ${JARFILE}"
+
+export VERSION_LIST="https://api.github.com/repos/DoubleScripts/ObamaBot/commits/master"
+
+export UPDATE_FILE="current_version.txt"
---
+++ ServerStartupScripts/update.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+. ./settings.sh
+
+latest_version="null"
+
+check_libraries() {
+    if ! command -v jq &> /dev/null
+    then
+        echo "jq not found. jq is required for JSON parsing"
+        exit
+    fi
+
+    if ! command -v ${JAVACMD} &> /dev/null
+    then
+        echo "Java command \"${JAVACMD}\" not found. It is required for running the server"
+        exit
+    fi
+}
+
+update_check() {
+  check_libraries
+
+  current_version="null"
+
+  if [ ! -f "${UPDATE_FILE}" ]; then
+    echo ${current_version} > "${UPDATE_FILE}"
+  else
+    current_version=$(<${UPDATE_FILE})
+  fi
+
+  latest_version=$(curl -JLs "${VERSION_LIST}" | jq -r .sha) # Gets the build key of the JSON result
+
+  echo "USING VERSION ${current_version} WHILE LATEST IS ${latest_version}"
+
+  if [ ! "${current_version}" == "${latest_version}" ] ; then
+
+    return 0
+  fi
+
+  return 1
+}
+
+update() {
+  copyJar=$1
+
+  echo "UPDATING TO NEW VERSION"
+
+  git pull
+
+  if ! $(buildJar $copyJar) ; then
+    echo "FAILED TO DOWNLOAD LATEST VERSION OF MINECRAFT SERVER"
+    return 1
+  else
+    echo "${latest_version}" > "${UPDATE_FILE}"
+    return 0
+  fi
+}
+
+buildJar() {
+  copyJar=$1
+
+  mvn clean package
+
+  mvnResult="$?"
+
+  if [ ! $mvnResult -eq 0 ] ; then
+    >&2 echo "Unable to build jar. Malformed java code? Running with previous version"
+  else
+    if [ "$copyJar" ] ; then
+      cp ./target/"${JARFILE}" ./"${JARFILE}"
+    fi
+  fi
+
+  echo "$mvnResult is result"
+
+  return $mvnResult
+}
+
+checkService() {
+  if P=$(pgrep $1)
+  then
+    echo "start"
+  else
+    echo "stop"
+  fi
+}
+
+updateAndRestart() {
+  serviceStatus=$(checkService "${SERVICE_NAME}")
+
+  if update_check ; then
+
+
+      if [ "$(update false)" ] ; then
+
+        sudo service "${SERVICE_NAME}" stop
+
+        cp ./target/"${JARFILE}" ./"${JARFILE}"
+
+        sudo service "${SERVICE_NAME}" "${serviceStatus}"
+
+      fi
+  fi
+}
