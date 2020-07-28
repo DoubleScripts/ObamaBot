71f1f02686db311c3c9ec2dd2d84cc290f5b4938
DoubleScripts <40070530+DoubleScripts@users.noreply.github.com>
2020-07-28 14:22:26 -0400

    Update settings to compensate for username change

--- ServerStartupScripts/settings.sh
+++ ServerStartupScripts/settings.sh
@@ -8,6 +8,6 @@ export SERVICE_NAME="obamabot"
 export RAM_USAGE="2G"
 export JVM_ARGUMENTS="-Xms${RAM_USAGE} -Xmx${RAM_USAGE} -jar ${JARFILE}"
 
-export VERSION_LIST="https://api.github.com/repos/DoubleScripts/ObamaBot/commits/master"
+export VERSION_LIST="https://api.github.com/repos/antaxiom/ObamaBot/commits/master"
 
 export UPDATE_FILE="current_version.txt"
